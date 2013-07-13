e47c4fc46578b1fb2bd2b85636df38112e30cccb
John Wiggins <jwiggins@enthought.com>
2013-07-13 13:49:11 -0500

    Refactor all the Conditionals into separate enamldefs. Test each editor.

---
+++ examples/auto_view/auto_view.py
@@ -0,0 +1,44 @@
+#
+# (C) Copyright 2013 Enthought, Inc., Austin, TX
+# All right reserved.
+#
+# This file is open source software distributed according to the terms in
+# LICENSE.txt
+#
+
+import datetime
+
+import enaml
+from enaml.qt.qt_application import QtApplication
+from traits.api import (HasTraits, Bool, Date, Enum, Float, Int, Str, Range,
+                        Str, Time)
+
+
+class AllTypes(HasTraits):
+    """ A simple class with all kinds of traits
+
+    """
+    boolean_value = Bool(True)
+    int_value = Int(42)
+    float_value = Float(3.141592)
+    enum_value = Enum("foo", "bar", "baz", "qux")
+    int_range_value = Range(low=0, high=10)
+    float_range_value = Range(low=0.0, high=1.0)
+    str_value = Str("Word")
+    date_value = Date(datetime.date.today())
+    time_value = Time(datetime.time())
+
+    def _anytrait_changed(self, name, old, new):
+        print name, "changed from", old, "to", new
+
+if __name__ == '__main__':
+    with enaml.imports():
+        from traits_enaml.widgets.auto_view import auto_window
+
+    all = AllTypes()
+
+    app = QtApplication()
+    view = auto_window(all)
+    view.show()
+
+    app.start()

2f481b54008b394332711f259b00d3cfadfbbe4e
Didrik Pinte <dpinte@enthought.com>
2013-11-15 13:38:56 +0100

    super basic test for the EnableCanvas widget

---
+++ traits_enaml/tests/test_enable_canvas.py
@@ -0,0 +1,54 @@
+import unittest
+
+from enable.api import Component
+from traits.api import HasTraits, Instance
+
+from traits_enaml.testing.enaml_test_assistant import EnamlTestAssistant
+
+class Model(HasTraits):
+
+    component = Instance(Component)
+
+
+class EnableCanvasTestCase(EnamlTestAssistant, unittest.TestCase):
+
+    def setUp(self):
+
+        EnamlTestAssistant.setUp(self)
+
+        enaml_source = """
+from enaml.widgets.api import MainWindow
+from traits_enaml.widgets.enable_canvas import EnableCanvas
+
+enamldef MainView(MainWindow):
+    attr model
+
+    EnableCanvas:
+        name = 'canvas'
+        component << model.component
+"""
+        self.component = Component()
+        self.model = Model(component=self.component)
+
+        view, toolkit_view = self.parse_and_create(
+            enaml_source, model=self.model
+        )
+
+        self.view = view
+
+    def tearDown(self):
+        self.component = None
+        self.view = None
+        self.model = None
+
+        EnamlTestAssistant.tearDown(self)
+
+    def test_using_enable_canvas_widget(self):
+
+        canvas = self.view.find('canvas')
+
+        with self.assertAtomChanges(canvas, 'component'):
+            self.model.component = Component()
+
+        canvas = None
+

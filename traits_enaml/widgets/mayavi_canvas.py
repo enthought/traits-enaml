3bfaf94fd5896cb5c1692fb91dc7e9c8358ad115
Ioannis Tziakos <ioannist@enthought.com>
2014-02-01 05:11:11 +0000

    rewrite MayaviCanvas as a TraitsView subclass

--- traits_enaml/widgets/mayavi_canvas.py
+++ traits_enaml/widgets/mayavi_canvas.py
@@ -2,68 +2,67 @@
 #  Copyright (c) 2013, Enthought, Inc.
 #  All rights reserved.
 #------------------------------------------------------------------------------
-from atom.api import Bool, Typed, observe, set_default
-
-from enaml.widgets.api import RawWidget
+from atom.api import Bool, Typed, observe, set_default, Constant
 from enaml.core.declarative import d_
 
-from traits.api import HasTraits, Instance, Bool as BoolTrait
-from traitsui.api import View, Item, Handler
-
+from traits.api import Instance, HasStrictTraits
+from traitsui.api import View, Item, UI
 from tvtk.pyface.scene_editor import SceneEditor
 from tvtk.pyface.scene_model import SceneModel
 
+from .traits_view import TraitsView
 
-class MayaviModel(HasTraits):
-    scene = Instance(SceneModel, ())
-    view = View(Item('scene', editor=SceneEditor(), resizable=True,
-                     show_label=False),
-                resizable=True)
-
-
-class ToolbarVisibilityHandler(Handler):
-    #: Should the toolbar be visible?
-    visible = BoolTrait
-
-    def position(self, info):
-        editor = info.ui.get_editors('scene')[0]
-        editor._scene._tool_bar.setVisible(self.visible)
+class MayaviModel(HasStrictTraits):
+    scene = Instance(SceneModel, args=())
+    view = View(
+        Item('scene', editor=SceneEditor(), resizable=True, show_label=False),
+        resizable=True)
 
 
-class MayaviCanvas(RawWidget):
-    """ A widget that displays a mayavi scene.
+class MayaviCanvas(TraitsView):
+    """ A traits view widget that displays a mayavi scene.
 
     :Attributes:
         **scene** = *d_(Typed(SceneModel))*
             The mayavi scene model to be displayed.
+        **show_toolbar** = *d_(Bool(True))*
+            If True, show the Mayavi toolbar.
 
     """
     #: The mayavi scene model to be displayed.
     scene = d_(Typed(SceneModel))
 
-    #: If True, show the Mayavi toolbar
+    #: If True, show the Mayavi toolbar.
     show_toolbar = d_(Bool(True))
 
-    #: The traits model that is used to create the mayavi UI
-    _model = MayaviModel()
+    #: The readonly instance of the model used for the traits_view.
+    model = d_(Typed(MayaviModel), writable=False)
 
-    #: Mayavi canvas' expand freely in width and height by default
+    #: The readonly instance of the view.
+    view = d_(Typed(View), writable=False)
+
+    #: Mayavi canvas expands freely in width and height by default.
     hug_width = set_default('ignore')
     hug_height = set_default('ignore')
 
-    def initialize(self):
-        super(MayaviCanvas, self).initialize()
-        if self.scene is not None:
-            self._model.scene = self.scene
-
     def create_widget(self, parent):
-        handler = ToolbarVisibilityHandler(visible=self.show_toolbar)
-        ui = self._model.edit_traits(kind='subpanel', handler=handler)
-        widget = ui.control
-        widget.setParent(parent)
+        control = super(MayaviCanvas, self).create_widget(parent)
+        self.show_toolbar_changed({'value': self.show_toolbar})
+        return control
 
-        return widget
+    def _default_model(self):
+        return MayaviModel(scene=self.scene)
+
+    def _default_view(self):
+        return self.model.trait_view()
 
     @observe('scene')
     def scene_changed(self, change):
-        self._model.scene = change['value']
+        self.model.scene = change['value']
+
+    @observe('show_toolbar')
+    def show_toolbar_changed(self, change):
+        ui = self.ui
+        if ui is not None and ui.control is not None:
+            editor = ui.get_editors('scene')[0]
+            editor._scene._tool_bar.setVisible(change['value'])

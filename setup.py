afe4dce6f365eb85185e8c4accb8e9af865907ed
Ioannis Tziakos <ioannist@enthought.com>
2014-02-20 17:02:54 +0000

    update all copyrights

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
-# Copyright (c) 2011-2013 by Enthought, Inc.
-# All rights reserved.
+#----------------------------------------------------------------------------
+#
+#  Copyright (c) 2013-14, Enthought, Inc.
+#  All rights reserved.
+#
+#  This software is provided without warranty under the terms of the BSD
+#  license included in /LICENSE.txt and may be redistributed only
+#  under the conditions described in the aforementioned license.  The license
+#  is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+#  Thanks for using Enthought open source!
+#
+#----------------------------------------------------------------------------
 
 from setuptools import setup, find_packages
 

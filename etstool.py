a9ac4fb6e228e2511582e2448e58806fd6c26a04
Ioannis Tziakos <mail@itziakos.gr>
2018-09-01 09:18:25 +0100

    Remove debugging command

--- etstool.py
+++ etstool.py
@@ -180,7 +180,6 @@ def test(runtime, toolkit, environment):
     with do_in_tempdir(files=['.coveragerc'], capture_files=['./.coverage*']):
         os.environ.update(environ)
         execute(commands, parameters)
-    execute(["edm run -e {environment} -- coverage report"], parameters)
     click.echo('Done test')
 
 
